c6de45b32e76c54be399c970ba4c49cf04df4df0
tqmallon <thomas_oo@hotmail.com>
2016-03-14 17:13:52 +0100

    White screen on startup

--- app/src/main/java/com/example/group21/balancebasket/Shoppinglist.java
+++ app/src/main/java/com/example/group21/balancebasket/Shoppinglist.java
@@ -1,15 +1,16 @@
 package com.example.group21.balancebasket;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.transition.Slide;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.ActionBarActivity;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.transition.Slide;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class Shoppinglist extends AppCompatActivity {
 
@@ -23,20 +24,16 @@ public class Shoppinglist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoppinglist);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         SLInput = (EditText) findViewById(R.id.SLInput);
         SList = (TextView) findViewById(R.id.SList);
         dbHandler = new MyDBHandler(this, null, null, 1);
-        printDatabase ();
-
+        printDatabase();
     }
 
-    public void printDatabase(){
-        String dbString = dbHandler.databaseToString();
-        SList.setText(dbString);
-        SLInput.setText("");
+    public void AddButtonClicked(View view) {
+        Products product = new Products(SLInput.getText().toString());
+        dbHandler.addProduct(product);
+        printDatabase();
     }
 
     public void RemoveButtonClicked (View view) {
@@ -45,10 +42,10 @@ public class Shoppinglist extends AppCompatActivity {
         printDatabase();
     }
 
-    public void AddButtonClicked(View view) {
-        Products products = new Products(SLInput.getText().toString());
-        dbHandler.addProduct(products);
-        printDatabase();
-
+    public void printDatabase(){
+        String dbString = dbHandler.databaseToString();
+        SList.setText(dbString);
+        SLInput.setText("");
     }
+
 }
